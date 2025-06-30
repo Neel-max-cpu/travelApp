86c2de1fc2223f814b849e22fa8e235aa327d451
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-06-30 18:01:08 +0530

    update :memo:

---
+++ src/components/Home/Navbar/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000]'>
+        Nav
+    </div>
+  )
+}
+
+export default Nav
